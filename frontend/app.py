95304d63b60b5bd96c866ed6b67a45214050ebef
Mayank Sharma <mayankhub@gmail.com>
2025-12-18 17:07:55 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import joblib
 
+from preprocessing import preprocess_payload
+
 # --------------------------------------------------
 # Page config
 # --------------------------------------------------
