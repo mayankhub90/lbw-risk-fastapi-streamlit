277c4c1899d02593cbc34f11abb13edfd1235c93
Mayank Sharma <mayankhub@gmail.com>
2025-12-23 11:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -481,10 +481,6 @@ worksheet = get_gsheet()
 # Add header if sheet is empty
 if worksheet.row_count == 0:
     worksheet.append_row(list(record.keys()))
-
-# Append data row
-worksheet.append_row(
-      value_input_option="USER_ENTERED"
 )
 
 st.success("✅ Record saved to Google Sheets")
